0c91fd0f0866e05b83651ca2ec61528f831dfbeb
vicente Valencia <varv280496@gmail.com>
2023-02-15 22:32:21 -0600

    creacion del componente Navbar

---
+++ src/componentes/Inicio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../estilos/Inicio.css'
+
+function Main() {
+    return (
+        <>
+            <div className="container"></div>
+        </>
+    )
+}
+
+export default Main
---
+++ src/componentes/MenuData.js
@@ -0,0 +1,31 @@
+export const MenuData = [
+    {
+        title: 'Inicio',
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-home-user'
+    },
+    {
+        title: 'Quienes Somos',
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-mug-hot'
+    },
+    {
+        title: 'Contacto',
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-address-book'
+    },
+    {
+        title: 'Platillos',
+        url: '#',
+        cName: 'nav-links',
+        icon: 'fa-solid fa-utensils'
+    },
+    {
+        title: 'Reservar',
+        url: '#',
+        cName: 'nav-links-mobile',
+    }
+]
